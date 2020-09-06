eceb460f38dd80798abc0c16a126a804370ac1db
vl4nkzz <vietanhtm@gmail.com>
2020-09-06 21:31:58 +0700

    Buoi1

---
+++ src/Buoi1/Bai1.java
@@ -0,0 +1,84 @@
+package Buoi1;
+
+import java.util.Scanner;
+
+public class Bai1 {
+    public static int searchDays(int month){
+        switch (month){
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                return 31;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                return 30;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int currentDay, currentMonth, currentYear;
+        int nextDay, nextMonth, nextYear;
+
+        System.out.print("Nhập ngày: ");
+        currentDay = sc.nextInt();
+        System.out.print("Nhập tháng: ");
+        currentMonth = sc.nextInt();
+        System.out.print("Nhập năm: ");
+        currentYear = sc.nextInt();
+
+        if (currentMonth!=2) {
+            if ((currentDay < (searchDays(currentMonth) - 1)) && (currentMonth != 12)) {
+                nextDay = currentDay + 1;
+                nextMonth = currentMonth;
+                nextYear = currentYear;
+            } else {
+                nextDay = 1;
+                nextMonth = currentMonth + 1;
+                nextYear = currentYear;
+            }
+            if ((currentDay == 31) && (currentMonth == 12)) {
+                nextDay = 1;
+                nextMonth = 1;
+                nextYear = currentYear + 1;
+            }
+
+        }
+        else{
+            if(((currentYear % 4 == 0) && (currentYear % 100 != 0)) || (currentYear % 400 == 0)){
+                if (currentDay<29){
+                    nextDay = currentDay + 1;
+                    nextMonth = currentMonth;
+                    nextYear = currentYear;
+                }
+                else{
+                    nextDay = 1;
+                    nextMonth = currentMonth + 1;
+                    nextYear = currentYear;
+                }
+            }
+            else{
+                if (currentDay<28){
+                    nextDay = currentDay + 1;
+                    nextMonth = currentMonth;
+                    nextYear = currentYear;
+                }
+                else{
+                    nextDay = 1;
+                    nextMonth = currentMonth + 1;
+                    nextYear = currentYear;
+                }
+            }
+        }
+        System.out.println("Ngày hôm sau: "+nextDay+"/"+nextMonth+"/"+nextYear);
+
+
+    }
+}
